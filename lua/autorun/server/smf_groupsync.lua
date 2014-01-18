e9846d70572ef91d8b423c0a627b6d7f96748007
GodzBro <btarm438@gmail.com>
2014-01-18 00:58:04 -0600

    Support for syncing SMF to ULX and sycing vip IP

--- lua/autorun/server/smf_groupsync.lua
+++ lua/autorun/server/smf_groupsync.lua
@@ -1,30 +1,49 @@
 --SMF Group Sync by Godz
+
 --Config
 local DB_HOST = ""
 local DB_USERNAME = ""
 local DB_PASSWORD = ""
-local DB_FORUM_DATABASE = ""
+local DB_SMF_DATABASE = ""
 local DB_PORT = 3306
 
-local Forum_Mod = "smf" --currently only supports SMF
+--Currently only supports SMF
+local Forum_Mod = "smf"
 
+--Should it sync according to SMF's groups?
+local SMF_to_ULX = true
+
+--Should it sync according to ULX's groups?
+local ULX_to_SMF = true
+
+--"SteamID" or "IP". If you have "IP" enabled, you don't have
+--to have Steam logins setup but this method is dangerous
+--if someone has the same IP of someone you are syncing.
+--Keep "SteamID" if possible.
+local Sync_Method = "SteamID"
+
+--Your ULX group must equal your SMF group's ID.
+--See Facepunch/Coderhire post for details.
 GroupID={
-    ["user"]=0, -- 0 is the default group for SMF and 2 is the default for MyBB
-    ["admin"]=12,
-    ["moderator"]=13,
-    ["owner"]=1,
-    ["donator"]=15
+    ["user"]=0,
+    ["donator"]=2,
+    ["operator"]=3,
+    ["moderator"]=4,
+    ["admin"]=5,
+	["owner"]=1
 }
 
---end of config
+--==========================================--
+--               END OF CONFIG			
+--==========================================--
+
+
 
 function log (msg)
-	ServerLog(msg.."\n")
+	ServerLog("[SMF Group Sync] "..msg.."\n")
 end
 
 require ("mysqloo")
-
-local dbconnected = false
 
 local db = mysqloo.connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_FORUM_DATABASE, DB_PORT)
 
@@ -38,9 +57,9 @@ function QueryDB( query, callback )
     end
     
     function q:onError(err, sql)
-        log("[SMF Group Sync] SMF group sync query errored.")
-        log("[SMF Group Sync] Query: ", sql)
-        log("[SMF Group Sync] Error: ", err)
+        log("Query errored.")
+        log("Query: ", sql)
+        log("Error: ", err)
 		
         if db:status() == 2 then
 			db:connect()
@@ -55,44 +74,79 @@ function QueryDB( query, callback )
 end
 
 function db:onConnectionFailed(err)
-    log("[SMF Group Sync] SMF group sync database connection failed.")
-    log("[SMF Group Sync] Error: ", err)
+    log("Database connection failed.")
+    log("Error: ", err)
 end
 
 function db:onConnected()
-    log("[SMF Group Sync] Group sync connected to SMF's MySQL (v"..db:serverVersion()..") database successfully.")
-    dbconnected = true
+    log("Connection to SMF's MySQL (v"..db:serverVersion()..") database successful.")
 end
+
+function splitPort( ip )
+	local pos = string.find( ip, ":" )
+	local str = string.sub( ip, 1, pos - 1 )
+	
+	return str
+end
+
+
+if Forum_Mod == "smf" and Sync_Method = "SteamID" then
+	querycheck = "SELECT * FROM smf_members WHERE member_name="..steamID..";"
+	queryB = "UPDATE smf_members SET id_group="..getID.." WHERE member_ip='"..splitPort(ply:IPAddress()).."';"
+elseif Forum_Mod == "smf" and Sync_Method == "IP" then
+	querycheck = "SELECT * FROM smf_members WHERE member_ip='"ply:IPAddress()"'"
+	queryB = "UPDATE smf_members SET id_group="..getID.." WHERE member_ip='"..splitPort(ply:IPAddress()).."';"
+elseif Forum_Mod == "mybb" then
+	timer.Simple(10, function() log("Error: MyBB is not supported yet. Talk with Godz if you would like to see this happen.") end)
+elseif Forum_Mod != "smf" then 
+	timer.Simple(10, function() log("Error: \""..Forum_Mod.."\" is not a valid forum mod.") end)
+elseif Sync_Method == "" or nil then
+	timer.Simple(10, function() log("Please choose a sync method.") end)
+else
+	timer.Simple(10, function() log("Something went wrong, please contact Godz.") end)
+end
+
+function FlipTable( table , NewTable )
+	local NewTable = {}
+	for k, v in next, table do
+		local key = k
+		local value = tostring(v)
+
+		table.v = k
+	end
+
+	return NewTable
+end
+
 
 function playerJoin( ply )
 	
    local steamID = ply:SteamID64()
    local getID = GroupID[ply:GetUserGroup()]
-   
-   if Forum_Mod =="smf" then
-   		Table_Prefix = "smf_"
-   		querycheck = "SELECT * FROM "..Table_Prefix.."members WHERE member_name="..steamID..";"
-   elseif Forum_Mod == "mybb" then
-   		Table_Prefix = "mybb_"
-   		querycheck = "SELECT * FROM "..Table_Prefix.."members WHERE member_name="..steamID..";"
-   end
-   
-    local query = querycheck
     
-    QueryDB(query, function(data)
-		PrintTable(data)
-        if data[1]["id_group"] != getID then
-            local queryB = "UPDATE "..Table_Prefix.."members SET id_group="..getID.." WHERE member_name="..steamID..";"
+    QueryDB(querycheck, function(data)
+		
+		if ULX_to_SMF then
+			if data[1]["id_group"] != getID then
+		
+				--made an empty function because I don't know if you can have an empty arg like this
+				QueryDB( queryB, function() end )		
 			
-			QueryDB(queryB, function() end)
-        end
+			end
+		end
+		
+		if SMF_to_ULX then
+			FlipTable(GroupID, ReversedGroupID)
+
+			ULib.ucl.addUser(ply:SteamID(), {}, {}, ReversedGroupID[data[1]["id_group"]])
+		end
     end)
     
 end
 hook.Add("PlayerInitialSpawn", "queryOnJoin", playerJoin)
 --hook.Add("UCLChanged", "queryOnGroupChange", playerJoin)
 
-concommand.Add( "sync_status", function( ply, cmd, args, str )
+concommand.Add( "sync_status", function()
 	log(db:status())
 end )
 
